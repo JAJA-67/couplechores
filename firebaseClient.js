6abc124195de439d273b74f96b108586ad2423dc
JAJA-67 <evahome2015@gmail.com>
2026-05-30 13:50:29 +0800

    Update firebaseClient.js

--- firebaseClient.js
+++ firebaseClient.js
@@ -1,19 +1,21 @@
-import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBhDanlI-QSqeMPXOz40KDJ9zHOrNeANTU",
-    authDomain: "couplechores.firebaseapp.com",
-    projectId: "couplechores",
-    storageBucket: "couplechores.appspot.com",
-    messagingSenderId: "1045191990257",
-    appId: "1:1045191990257:web:566f0394ca1c651d808250",
-    measurementId: "G-T6VT7QB2HZ"
+  apiKey: "AIzaSyDnRIhSE6f6Z0Ccs4CGbjP49BkfJSCrzw8",
+  authDomain: "lovebank-67a55.firebaseapp.com",
+  projectId: "lovebank-67a55",
+  storageBucket: "lovebank-67a55.firebasestorage.app",
+  messagingSenderId: "218336388779",
+  appId: "1:218336388779:web:3b19de4f95779f615d39ee",
+  measurementId: "G-4E5ZP77P0W"
 };
-  
-const apps = getApps();
-if (!apps.length) {
-    initializeApp(firebaseConfig);
-}
-  
-export const auth = getAuth();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
